3332b07e69fb0e4118b4ae5456cfa8a85291ae4d
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-02-10 16:17:12 -0700

    Add: add error display if there is no name found, fix: change useEffects so they do not render constanty

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,9 +1,10 @@
 import "./ui.css";
 import "../node_modules/figma-plugin-ds/dist/figma-plugin-ds.css";
-import { useRef, useState, useEffect, MouseEvent, KeyboardEvent } from "react";
+import { useRef, useState, useEffect, MouseEvent } from "react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import fetch from "node-fetch";
+import { SearchList } from "./SearchList";
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -12,6 +13,9 @@ function App() {
   const [countryISO, setCountryISO] = useState<string>("");
   const [geoJSON, setGeoJSON] = useState<string>("");
   const [svgContent, setSvgContent] = useState<string>("");
+  const [countriesList, setCountriesList] = useState<string>("");
+  const [renderList, setRenderList] = useState(true);
+  const [error, setError] = useState(false);
   const GeoJSON2SVG = require("geojson2svg").GeoJSON2SVG;
 
   const fetchCountryISONumber = async (): Promise<void> => {
@@ -22,16 +26,39 @@ function App() {
       const data = await response.json();
       if (response.ok) {
         setCountryISO(data[0]?.cca3);
+        setError(false);
+        setCountryName("");
       } else {
+        setError(true);
         throw new Error("No ISO code for country found" + response.status);
       }
     } catch (err) {
+      setError(true);
+      setCountryName("");
       console.log(err);
     }
   };
 
+  const fetchNames = (value: String) => {
+    fetch("https://restcountries.com/v3.1/all?fields=name")
+      .then((response) => response.json())
+      .then((json) => {
+        const names = json.filter((countries) => {
+          let result =
+            value &&
+            countries &&
+            countries.name.common &&
+            countries.name.common.toLowerCase().includes(value);
+
+          return result;
+        });
+        setCountriesList(names);
+      });
+  };
+
   const fetchGeoJSON = async (): Promise<void> => {
     try {
+      console.log("code", countryISO);
       const responseGeoBundaries = await fetch(
         `https://www.geoboundaries.org/api/current/gbOpen/${countryISO}/ADM0/`
       );
@@ -84,23 +111,33 @@ function App() {
     if (countryISO) {
       fetchGeoJSON();
     }
+  }, [countryISO]);
+
+  useEffect(() => {
+    if (countryName) {
+      fetchCountryISONumber();
+    }
+  }, [countryName]);
+
+  useEffect(() => {
     if (geoJSON) {
       convertGeoJSONToSVGPath();
       if (svgContent) {
         onCreateFigma(svgContent);
       }
     }
-  }, [countryISO, geoJSON]);
+  }, [geoJSON, svgContent]);
 
   const submitByEnterButton = (event) => {
     if (event.keyCode === 13) {
       onSubmit(event);
+      setRenderList(false);
     }
   };
 
   const onSubmit = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    await fetchCountryISONumber();
+    setCountryName(inputRef.current?.value);
   };
 
   const onCreateFigma = async (svgContent: String) => {
@@ -110,12 +147,16 @@ function App() {
     );
   };
 
+  const handleCountryClick = () => {
+    setRenderList(false);
+  };
   const onChange = async () => {
     if (inputRef.current?.value === "") {
       setDisabled(false);
     } else {
       setDisabled(true);
-      setCountryName(inputRef.current?.value);
+      setError(false);
+      fetchNames(inputRef.current?.value);
     }
   };
   return (
@@ -125,18 +166,34 @@ function App() {
       </header>
       <main>
         <label htmlFor="input">Country name:</label>
-        <div className="input input--with-icon">
-          <div className="icon icon--search"></div>
-          <input
-            type="text"
-            className="input__field"
-            placeholder="Country.."
-            id="input"
-            ref={inputRef}
-            onChange={onChange}
-            onKeyDown={(e) => submitByEnterButton(e)}
-          />
+        <div>
+          <div className="input input--with-icon">
+            <div className="icon icon--search"></div>
+            <input
+              type="text"
+              className="input__field"
+              placeholder="Country.."
+              id="input"
+              ref={inputRef}
+              onChange={onChange}
+              onKeyDown={(e) => submitByEnterButton(e)}
+            />
+            {error && (
+              <p className="error" style={{ fontSize: "8px", color: "red" }}>
+                Please enter correct country name.
+              </p>
+            )}
+          </div>
         </div>
+        {renderList && countriesList && countriesList.length > 0 ? (
+          <SearchList
+            countries={countriesList}
+            inputRef={inputRef}
+            onClick={handleCountryClick}
+          />
+        ) : (
+          ""
+        )}
         <div>
           {svgContent ? (
             <svg
@@ -145,13 +202,13 @@ function App() {
               height="200"
               xmlns="http://www.w3.org/2000/svg"
               style={{
-                fill: "red",
+                fill: "lightCoral",
                 border: "1px solid black",
               }}
             >
               <path d={`${svgContent}`} />
             </svg>
-          ) : countryName ? (
+          ) : countryName.length > 0 && !error ? (
             <p>Loading GeoJSON data...</p>
           ) : (
             ""
