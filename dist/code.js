ffc588976ff90b666b5e68b8f65958e0c5ba9a40
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-02-13 11:48:29 -0700

    UI change

--- dist/code.js
+++ dist/code.js
@@ -1,17 +1,1 @@
-figma.showUI(__html__, { themeColors: false, height: 350, width: 300 });
-figma.ui.onmessage = (message) => {
-    if (message.type === "no-preview") {
-        figma.ui.resize(300, 350);
-    }
-    else if (message.type === "preview") {
-        figma.ui.resize(650, 400);
-    }
-    else if (message.type === "create-map") {
-        console.log("Got this from the UI", message);
-        const svgString = `<svg xmlns="http://www.w3.org/2000/svg"><path d='${message.svgContent}' fill="#D79393" transform="scale(1.2)"/></svg>`;
-        const svgNode = figma.createNodeFromSvg(svgString);
-        figma.currentPage.appendChild(svgNode);
-        figma.viewport.scrollAndZoomIntoView([svgNode]);
-        figma.closePlugin();
-    }
-};
+figma.showUI(__html__,{themeColors:!1,height:350,width:300}),figma.ui.onmessage=e=>{if("no-preview"===e.type)figma.ui.resize(300,350);else if("preview"===e.type)figma.ui.resize(650,400);else if("create-map"===e.type){console.log("Got this from the UI",e);const i=`<svg xmlns="http://www.w3.org/2000/svg"><path d='${e.svgContent}' fill="#D79393" transform="scale(1.2)"/></svg>`,o=figma.createNodeFromSvg(i);figma.currentPage.appendChild(o),figma.viewport.scrollAndZoomIntoView([o]),figma.closePlugin()}};
